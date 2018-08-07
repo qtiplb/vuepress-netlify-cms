c911f962c334deda2c6fa5c89cf61cbd1682bcdf
Quentin Lavallée-B <quentin.lavallee@numigi.com>
2018-08-07 15:14:09 -0400

    Side

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,19 @@ module.exports = {
   themeConfig: {
     docsDir: 'docs',
     repo: 'andreliem/vuepress-netlify-cms',
+    sidebar: [
+      {
+        title: 'Group 1',
+        collapsable: false,
+        children: [
+          '/'
+        ]
+      },
+      {
+        title: 'Group 2',
+        children: [ /* ... */ ]
+      }
+    ]
     sideBar: 'auto',
     activeHeaderLinks: true, // Default: true
     displayAllHeaders: true,
